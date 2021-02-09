bc4988ffeb2226d1635974697096a61a6d900654
Valerian <valgerin@hotmail.fr>
2021-02-09 15:59:02 +0100

    setting up controller to create user

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,39 @@
+const user = require("../models/user.model");
+const { findUserPerEmail } = require("../queries/user.query");
+
+exports.newUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    //First checking if the email is provided from the form
+    const availableEmail = email.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/);
+
+    if (availableEmail !== null) {
+      //Then checking if the email already exist since it's unique
+      const emailAlreadyExist = await findUserPerEmail(email);
+
+      if (emailAlreadyExist) {
+        //If it already exist can't create the user
+        res.status(409);
+        res.json("Cet email est deja utilisé");
+      } else {
+        //Calling statics method on the model to hash the password
+        const hashedPassword = await user.hashPassword(password);
+
+        const newUser = new user({
+          _id: null,
+          name: name,
+          logInfos: {
+            email: email,
+            password: hashedPassword,
+          },
+        });
+        res.status(200);
+        newUser.save();
+      }
+    }
+    res.end()
+  } catch (error) {
+    throw error;
+  }
+};
