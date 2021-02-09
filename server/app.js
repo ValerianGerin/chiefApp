b256252399dfb21ac108b55d617da7191095d654
Valerian <valgerin@hotmail.fr>
2021-02-09 15:58:04 +0100

    import and using the router

--- server/app.js
+++ server/app.js
@@ -1,11 +1,15 @@
+require("./database/db.connect");
 const express = require("express");
 const app = express();
+const router = require("./routes")
 const PORT = process.env.PORT || 3000
 exports.app = app
 
 
 require("./middlewares/middleware");
 
+
+app.use(router)
 
 app.listen(PORT, ()=>{
   console.log(`App running on ${PORT} port`)
