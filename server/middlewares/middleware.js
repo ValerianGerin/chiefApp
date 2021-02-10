1a463a2c02b44abde8c6b69eeb03bf39622974b1
Valerian <valgerin@hotmail.fr>
2021-02-10 12:12:35 +0100

    rename from middleware to global.middleware

--- server/middlewares/middleware.js
+++
@@ -1,7 +0,0 @@
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const { app } = require("../app");
-
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
