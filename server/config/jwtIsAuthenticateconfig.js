c66b66775f870656b7670f535f2ff24d1c975f3a
Valerian <valgerin@hotmail.fr>
2021-02-10 15:28:51 +0100

    adding comments

--- server/config/jwtIsAuthenticateconfig.js
+++ server/config/jwtIsAuthenticateconfig.js
@@ -2,25 +2,29 @@ const jwt = require("jsonwebtoken");
 const { secret } = require("./jwtSecret.config");
 const { findUserPerID } = require("../queries/user.query");
 
-exports.isAuthenticate = (req, res, next) => {
-  const token = req.headers.Authorization;
+/*Function used on protected routes too check if token and token is valid and create a 
+user key on req that container the user info since the decodedToken contain the userID*/
+exports.isAuthenticate = async (req, res, next) => {
+  const token = req.headers.autorization;
+
   if (token) {
     try {
-      const decodedToken = jwt.verify(token, secret);
-      const user = await findUserPerID(decodedToken.payload)
-      if(user){
-        req.user = user
-        next()
+      const decodedToken = await jwt.verify(token, secret);
 
+      const user = await findUserPerID(decodedToken.payload);
+      if (user) {
+        req.user = user;
+        next();
       } else {
-        res.status(401);
-        next()
+        res.status(404);
+        next();
       }
-    } catch (error) {}
-  } else{
-    next()
+    } catch (error) {
+      res.status(401);
+      res.end();
+      throw error;
+    }
+  } else {
+    next();
   }
 };
-
-
-
