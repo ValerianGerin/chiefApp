80f54ce12fc645d398505bc2927d4b71163a1708
Valerian <valgerin@hotmail.fr>
2021-02-10 12:10:59 +0100

    setting up auth routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,9 @@
 const router = require("express").Router();
 const userRouter = require("./user.routes");
+const authRouter = require("./auth.routes");
 
 router.use("/user", userRouter)
-//router.use("/auth")
+router.use("/auth", authRouter)
 //router.use("/recipes")
 
 
