c790e10e9af036a2328da94a3dbc98857a96c065
Valerian <valgerin@hotmail.fr>
2021-02-10 12:11:53 +0100

    adding findUserPerID query

--- server/queries/user.query.js
+++ server/queries/user.query.js
@@ -1,9 +1,17 @@
 const user = require("../models/user.model");
 
+
+exports.findUserPerID = (id)=>{
+  return user.findById(id).exec()
+}
+
 exports.findUserPerEmail = (email) => {
 
   return user.findOne({ "logInfos.email": email }).exec();
 };
+
+
+
 
 
 
