8c9ac591b67a45e2cbebc6143c15dc7ce56a52f7
Valerian <valgerin@hotmail.fr>
2021-02-10 15:29:41 +0100

    using isAuthenticate middleware on protected routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,12 @@
+const { isAuthenticate } = require("../config/jwtIsAuthenticateconfig");
 const router = require("express").Router();
 const userRouter = require("./user.routes");
 const authRouter = require("./auth.routes");
+const userRecipesRouter = require("./userRecipes.routes");
 
-router.use("/user", userRouter)
-router.use("/auth", authRouter)
+router.use("/user", userRouter);
+router.use("/auth", authRouter);
+router.use("/protected", isAuthenticate, userRecipesRouter);
 //router.use("/recipes")
 
-
-module.exports = router
+module.exports = router;
