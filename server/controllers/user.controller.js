eae01a3e3ed69ec1b38ffec784f0116caf3d7431
Valerian <valgerin@hotmail.fr>
2021-02-10 15:34:05 +0100

    moving trycatch block and setting up 424 code status if ressource can't be create

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -2,21 +2,21 @@ const user = require("../models/user.model");
 const { findUserPerEmail } = require("../queries/user.query");
 
 exports.newUser = async (req, res) => {
-  try {
-    const { name, email, password } = req.body;
+  const { name, email, password } = req.body;
 
-    //First checking if the email is provided from the form
-    const availableEmail = email.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/);
+  //First checking if the email is provided from the form
+  const availableEmail = email.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/);
 
-    if (availableEmail !== null) {
-      //Then checking if the email already exist since it's unique
-      const emailAlreadyExist = await findUserPerEmail(email);
+  if (availableEmail !== null) {
+    //Then checking if the email already exist since it's unique
+    const emailAlreadyExist = await findUserPerEmail(email);
 
-      if (emailAlreadyExist) {
-        //If it already exist can't create the user
-        res.status(409);
-        res.json("Cet email est deja utilisé");
-      } else {
+    if (emailAlreadyExist) {
+      //If it already exist can't create the user
+      res.status(409);
+      res.json("Cet email est deja utilisé");
+    } else {
+      try {
         //Calling statics method on the model to hash the password
         const hashedPassword = await user.hashPassword(password);
 
@@ -28,12 +28,14 @@ exports.newUser = async (req, res) => {
             password: hashedPassword,
           },
         });
-        res.status(200);
+        res.status(201);
         newUser.save();
+      } catch (error) {
+        res.status(424);
+        res.end();
+        throw error;
       }
     }
-    res.end()
-  } catch (error) {
-    throw error;
   }
+  res.end();
 };
