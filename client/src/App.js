9310ca3b58875c90a17ccd721cb5750b7841a21c
Valerian <valgerin@hotmail.fr>
2021-02-15 16:13:25 +0100

    setting up login, logout methods and passing thought AuthProvider

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import { Navbar, Footer } from "./components";
 import { Switch, Route } from "react-router-dom";
+import { AuthProvider } from "./context/AuthContext";
 import "./index.scss";
 
 const App = () => {
+  const [loggedStatus, setLoggedStatus] = useState({
+    isLogged: false,
+    token: null,
+    user: null,
+  });
+
+  const login = () => {
+    const token = localStorage.getItem("authorization");
+    const user = JSON.parse(window.atob(token.split(".")[1]));
+    setLoggedStatus({ isLogged: true, token: token, user: user });
+  };
+
+  const logout = () => {
+    localStorage.removeItem("authorization");
+    setLoggedStatus({ isLogged: false, token: null });
+  };
+
   return (
     <>
-      <Navbar />
-      <Switch>
-        <Route to="/"></Route>
-      </Switch>
-      <Footer />
+      <AuthProvider value={{ loggedStatus, login, logout }}>
+        <Navbar />
+        <Switch>
+          <Route to="/"></Route>
+        </Switch>
+        <Footer />
+      </AuthProvider>
     </>
   );
 };
