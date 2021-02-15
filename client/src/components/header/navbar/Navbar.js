8d0c58dc257d69f0363e9fc50396e540e2554d8c
Valerian <valgerin@hotmail.fr>
2021-02-15 13:15:38 +0100

    remove index.scss and setting up scss module

--- client/src/components/header/navbar/Navbar.js
+++ client/src/components/header/navbar/Navbar.js
@@ -1,11 +1,20 @@
 import React from "react";
-import "../../../index.scss";
+import style from "./Navbar.module.scss";
 
 
 const Navbar = () => {
   return (
-    <div>
-      
+    <div className={style.navbar}>
+      <div>logo</div>
+      <ul>
+        <li>Recettes!</li>
+        <li>Connexion!!</li>
+        <li>Inscription!!</li>
+        <li>Deconnexion!!!</li>
+        <li>Profil!!!</li>
+
+      </ul>
+      <input/>
     </div>
   )
 }
