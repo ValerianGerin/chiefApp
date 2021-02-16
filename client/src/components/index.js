a2cee08410268763a826476d3a8f8085c5408c63
Valerian <valgerin@hotmail.fr>
2021-02-16 15:47:48 +0100

    setting up homepage comp

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,2 +1,4 @@
 export { default as Navbar } from "./header/navbar/Navbar";
 export { default as Footer } from "./footer/Footer";
+export { default as Searchbar } from "./header/searchbar/Searchbar";
+export { default as Homepage } from "./homepage/Homepage";
