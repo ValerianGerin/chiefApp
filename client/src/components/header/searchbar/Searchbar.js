e85972c261ff8a5276d76ba0674269af072d7b34
Valerian <valgerin@hotmail.fr>
2021-02-16 15:49:41 +0100

    setting up searchbar

--- client/src/components/header/searchbar/Searchbar.js
+++ client/src/components/header/searchbar/Searchbar.js
@@ -1,12 +1,14 @@
 import React from "react";
-
+import style from "./Searchbar.module.scss";
+import { BiSearchAlt } from "react-icons/bi";
 
 const Searchbar = () => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <>
+      <BiSearchAlt className={style.collapsedSearchbar} size={45}></BiSearchAlt>
+      <input className={style.searchbar} type="text" />
+    </>
+  );
+};
 
-export default Searchbar
+export default Searchbar;
