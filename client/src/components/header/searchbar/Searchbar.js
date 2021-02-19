4937a36b6352eba19e436e1ab8e3f769a84dd3eb
Valerian <valgerin@hotmail.fr>
2021-02-19 13:10:01 +0100

    removing icons from searchbar

--- client/src/components/header/searchbar/Searchbar.js
+++ client/src/components/header/searchbar/Searchbar.js
@@ -1,11 +1,11 @@
 import React from "react";
 import style from "./Searchbar.module.scss";
-import { BiSearchAlt } from "react-icons/bi";
+
 
 const Searchbar = () => {
   return (
     <>
-      <BiSearchAlt className={style.collapsedSearchbar} size={45}></BiSearchAlt>
+     
       <input className={style.searchbar} type="text" />
     </>
   );
