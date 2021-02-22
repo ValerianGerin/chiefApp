1079865ef6d856f9b0492cd8e3ec7ff33eab98e0
Valerian <valgerin@hotmail.fr>
2021-02-22 16:43:05 +0100

    delting consolelog

--- client/src/components/footer/footer-pages/policy-of-use-of-data/PolicyOfUseOfData.js
+++ client/src/components/footer/footer-pages/policy-of-use-of-data/PolicyOfUseOfData.js
@@ -1,8 +1,8 @@
 import React from "react";
 import style from "./PolicyOfUseOfData.module.scss";
 
-const PolicyOfUseOfData = (props) => {
-  console.log(props);
+const PolicyOfUseOfData = () => {
+
   return (
     <div className={style.container}>
       <section>
