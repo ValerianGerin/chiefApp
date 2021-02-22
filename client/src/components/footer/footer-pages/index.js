955c227c37265c52a568a40a51fc772dc0035061
Valerian <valgerin@hotmail.fr>
2021-02-22 16:40:27 +0100

    export all component from the same index

--- client/src/components/footer/footer-pages/index.js
+++
@@ -1,3 +0,0 @@
-export { default as PolicyOfUseOfData } from "./policy-of-use-of-data/PolicyOfUseOfData";
-export { default as LegalNotice } from "./legal-notice/LegalNotice";
-export { default as TermsOfServices } from "./terms-of-service/TermsOfServices";
--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,3 +2,11 @@ export { default as Navbar } from "./header/navbar/Navbar";
 export { default as Footer } from "./footer/footer-bar/Footer";
 export { default as Searchbar } from "./header/searchbar/Searchbar";
 export { default as Homepage } from "./homepage/Homepage";
+
+export { default as PolicyOfUseOfData } from "./footer/footer-pages/policy-of-use-of-data/PolicyOfUseOfData";
+export { default as LegalNotice } from "./footer/footer-pages/legal-notice/LegalNotice";
+export { default as TermsOfServices } from "./footer/footer-pages/terms-of-service/TermsOfServices";
+
+export { default as Login } from "./forms/login/Login";
+export { default as Register } from "./forms/register/Register";
+export { default as AddRecipe } from "./forms/add-recipe/AddRecipe";
