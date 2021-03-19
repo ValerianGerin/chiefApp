6784098414554543cd9aeeb36681a455d95c4111
Valerian <valgerin@hotmail.fr>
2021-03-19 12:31:46 +0100

    adding findById route and query

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const { newUser } = require("../controllers/user.controller");
+const { findUserPerID } = require("../queries/user.query");
 
 router.post("/new", newUser);
+router.get("/:id", findUserPerID);
 
 module.exports = router;
